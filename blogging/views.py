a9970a0b451a63a2e5d924f081190ae52d19d1b4
colephalen <colephalen@gmail.com>
2019-11-12 21:30:30 -0800

    activity 07

--- blogging/views.py
+++ blogging/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-
 from blogging.models import Post
 
 
@@ -15,12 +14,23 @@ def detail_view(request, post_id):
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
 
-
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
+
+# the following is from the lesson content but im unsure how it's different from above
+# everything on the lesson content is already on git-hub. im just scanning and reading...
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     body = template.render(context)
+#     return HttpResponse(body, content_type="text/html")
+
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
