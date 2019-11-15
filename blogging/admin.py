9482ac345da73aeabce305757b70921a20e36bf4
colephalen <colephalen@gmail.com>
2019-11-14 19:26:17 -0800

    i think my admin page does everything it should

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,8 +1,24 @@
 # django_blog_2/blogging/admin.py
 
+# got this from the week's class, unsure what the breakdown is on each class and how they work
+
 from django.contrib import admin
 from blogging.models import Post, Category
 
+# admin.site.register(Post)
+# admin.site.register(Category)
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
